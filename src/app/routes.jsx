bdcdc4099a53464e20b20eb3c2170dcae9a355e9
Tosin <tosin.yusuf@justice.gov.uk>
2026-02-09 16:56:22 +0000

    Added PFLRS pages

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -3,7 +3,10 @@ import FeeSchemes from '../pages/FeeSchemes';
 import FamilyAdvocacySchemePage from '../pages/FamilyAdvocacySchemePage';
 import PrivateFamilyLawRepresentationSchemePage from '../pages/PrivateFamilyLawRepresentationSchemePage';
 import CareProceedingsGraduatedFeeSchemePage from '../pages/CareProceedingsGraduatedFeeSchemePage';
+import CertificationDatePage from '../pages/CertificationDatePage'
 import AspectOfWork from '../pages/AspectOfWork';
+import ProcessCompletePage from '../pages/ProcessCompletePage';
+import ProviderLocationDatePage from '../pages/ProviderLocationPage';
 
 const AppRoutes = () => {
   return (
@@ -13,6 +16,9 @@ const AppRoutes = () => {
       <Route path="/private-family-law-representation-scheme" element={<PrivateFamilyLawRepresentationSchemePage />} />
       <Route path="/care-proceedings-graduated-fee-scheme" element={<CareProceedingsGraduatedFeeSchemePage />} />
       <Route path="/aspect-of-work" element={<AspectOfWork />} />
+      <Route path="/certification-date" element={<CertificationDatePage />} />
+      <Route path="/provider-location" element={<ProviderLocationDatePage />} />
+      <Route path="/process-complete" element={<ProcessCompletePage />} />
     </Routes>
   );
 };
--- src/pages/PrivateFamilyLawRepresentationSchemePage.jsx
+++ src/pages/PrivateFamilyLawRepresentationSchemePage.jsx
@@ -1,28 +1,102 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const PrivateFamilyLawRepresentationSchemePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="govuk-width-container">
-            <main className="govuk-main-wrapper">
-                <h1 className="govuk-heading-xl">Private Family Law Representation Scheme (PFLRS)</h1>
-                
-                <p className="govuk-body">
-                    This is the Private Family Law Representation Scheme page.
-                </p>
+  const [selectedRadio, setSelectedRadio] = useState("");
 
-                <button
-                    className="govuk-button govuk-button--secondary"
-                    data-module="govuk-button"
-                    onClick={() => navigate(-1)}
-                >
-                    Back
-                </button>
-            </main>
+  const proceedingsOptions = [
+    {
+      value: "Children",
+      label: "Children",
+    },
+    {
+      value: "Finance",
+      label: "Finance",
+    },
+    {
+      value: "DomesticAbuse",
+      label: "Domestic Abuse",
+    },
+    {
+      value: "Excluded",
+      label: "Excluded from PFLRS",
+    },
+  ];
+
+  const handleRadioChange = (e) => {
+    setSelectedRadio(e.target.value);
+  };
+
+  const handleContinue = () => {
+    if (selectedRadio === "Excluded") {
+      navigate("/process-complete");
+    } else {
+      navigate("/certification-date");
+    }
+  };
+
+  return (
+    <div className="govuk-width-container">
+      <main className="govuk-main-wrapper">
+        <h1 className="govuk-heading-xl">
+          Private Family Law Representation Scheme (PFLRS)
+        </h1>
+
+        <div className="govuk-form-group">
+          <fieldset className="govuk-fieldset">
+            <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
+              <h1 className="govuk-fieldset__heading">
+                Select proceedings type
+              </h1>
+            </legend>
+            <div className="govuk-radios" data-module="govuk-radios">
+              {proceedingsOptions
+                .filter((option) => option.value !== "")
+                .map((option) => (
+                  <div key={option.value} className="govuk-radios__item">
+                    <input
+                      className="govuk-radios__input"
+                      id={option.value}
+                      name="proceedingsTypes"
+                      type="radio"
+                      value={option.value}
+                      checked={selectedRadio === option.value}
+                      onChange={handleRadioChange}
+                    />
+                    <label
+                      className="govuk-label govuk-radios__label"
+                      htmlFor={option.value}
+                    >
+                      {option.label}
+                    </label>
+                  </div>
+                ))}
+            </div>
+          </fieldset>
         </div>
-    );
+
+        <div className="govuk-button-group">
+          <button
+            className="govuk-button"
+            data-module="govuk-button"
+            onClick={handleContinue}
+            disabled={!selectedRadio}
+          >
+            Next
+          </button>
+          <button
+            className="govuk-button govuk-button--secondary"
+            data-module="govuk-button"
+            onClick={() => navigate(-1)}
+          >
+            Back
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default PrivateFamilyLawRepresentationSchemePage;
